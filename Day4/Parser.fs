39e12de6d628a87fdd527fda2d1f0ee15b418036
Dominick Schroer <dominick@schroer.ca>
2020-12-04 14:13:12 -0500

    Cleaned up parser

--- Day4/Parser.fs
+++ Day4/Parser.fs
@@ -4,18 +4,17 @@ open Common.Regex
 open Common.Files
 open Day4.Passport
 
-let parsePassport(text: string) =
+let parseField part =
+    match part with
+    | Regex @"(.+):(.+)" [ name; value ] -> Some { Name = name; Value = value }
+    | _ -> None
+
+let parsePassport (text: string) =
     text.Split(' ', '\n')
-    |> Array.choose (fun part ->
-        match part with
-        | Regex @"(.+):(.+)" [name; value] -> Some {Name = name; Value = value}
-        | _ -> None)
+    |> Array.choose parseField
     |> Passport
 
-let parsePassports(text: string) =
-    text.Split("\n\n")
-        |> Array.map parsePassport
+let parsePassports (text: string) =
+    text.Split("\n\n") |> Array.map parsePassport
 
-let input() =
-    readAll "Passports.txt" parsePassports
-    
+let input () = readAll "Passports.txt" parsePassports
