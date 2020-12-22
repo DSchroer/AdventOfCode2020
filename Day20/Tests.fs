77cfd25e7581a60b5cc3124abba21bc4ce563c3f
Dominick Schroer <dominick@schroer.ca>
2020-12-22 17:05:42 -0500

    Completed day 20

--- Day20/Tests.fs
+++ Day20/Tests.fs
@@ -1,54 +1,122 @@
-//module Tests
-//
-//open System
-//open Day20
-//open Xunit
-//open Common.Files
-//open Tile
-//
-//let input() =
-//    readAll "Tiles.txt" (fun all ->
-//        let blocks = all.Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
-//        blocks |> Array.map parseTile) |> Array.toList
-//
-//type Tiles = Tile list
-//    
-//let connections (tiles: Tiles) =
-//    [
-//        for tile in tiles do
-//            let connections = tiles
-//                              |> List.filter (fun t -> t.Id <> tile.Id)
-//                              |> List.filter (fun t -> Tile.CanConnect tile t)
-//            yield (tile, connections)
-//            ]
-//
-//[<Fact>]
-//let ``Can rotate images``() =
-//    let img = Image(["..#"; "..."; "..."; "..."])
-//    Assert.Equal(img.Top, img.Rotate().Right)
-//    Assert.Equal(img.Top, img.Rotate().Rotate().Bottom)
-//    Assert.Equal(img.Top, img.Rotate().Rotate().Rotate().Left)
-//    Assert.Equal(img.Top, img.Rotate().Rotate().Rotate().Rotate().Top)
-//
-//[<Fact>]
-//let ``Has one way to flip``() =
-//    let img = Image(["1234"; "5678"; "9*@#"; "%^&("])
-//    let flip = img.Flip ()
-//    Assert.Equal(0, (Set.intersect (Set img.Sides) (Set flip.Sides)).Count)
-//
-//[<Fact>]
-//let ``Can find corners`` () =
-//    let sides =  input()
-//                 |> connections
-//                 |> List.filter (fun (_, c) -> c.Length = 2)
-//                 |> List.map (fun (t, _) -> t)
-//                 |> Seq.toList
-//                 
-//    Assert.Equal(4, sides.Length)
-//    Assert.Equal(12519494280967L, sides |> List.map (fun t -> int64 t.Id) |> List.reduce (*))
-//    
-//[<Fact>]
-//let ``Can find all connections`` () =
-//    let sides =  input() |> connections |> Seq.toList
-//    let g = Grid(sides).Print().Print()
-//    ()
+module Tests
+
+open System
+open Day20
+open Xunit
+open Common.Files
+open Tile
+
+let input() =
+    readAll "Tiles.txt" (fun all ->
+        let blocks = all.Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+        blocks |> Array.map parseTile) |> Array.toList
+
+type Tiles = Tile list
+    
+let connections (tiles: Tiles) =
+    [
+        for tile in tiles do
+            let connections = tiles
+                              |> List.filter (fun t -> t.Id <> tile.Id)
+                              |> List.filter (fun t -> Tile.CanConnect tile t)
+            yield (tile, connections)
+            ]
+    
+
+[<Fact>]
+let ``Can find corners`` () =
+    let corners =  input()
+                 |> connections
+                 |> List.filter (fun (_, c) -> c.Length = 2)
+                 |> List.map (fun (t, _) -> t)
+                 |> Seq.toList
+                 
+    Assert.Equal(4, corners.Length)
+    Assert.Equal(12519494280967L, corners |> List.map (fun t -> int64 t.Id) |> List.reduce (*))
+    
+let findRight (image: Image) (tiles: Tiles) =
+    let right = image.Right
+    let tile = tiles |> List.find (fun t -> t.AllSides.Contains right)
+    let selected = if Set(tile.Image.Sides).Contains right then tile.Image else tile.Flipped
+    
+    let nextImage = 
+                    if right = selected.Top then selected.Flip().Rotate().Rotate().Rotate()
+                    else if right = selected.Left then selected.Flip().Rotate().Rotate()
+                    else if right = selected.Bottom then selected.Flip().Rotate()
+                    else if right = selected.Right then selected.Flip()
+                    else failwith "Unsupported alignment"
+    (tile.Id, nextImage)
+    
+let findBottom (image: Image) (tiles: Tiles) =
+    let bottom = image.Bottom
+    let tile = tiles |> List.find (fun t -> t.AllSides.Contains bottom)
+    let selected = if Set(tile.Image.Sides).Contains bottom then tile.Image else tile.Flipped
+    
+    let nextImage = 
+                    if bottom = selected.Top then selected.Flip()
+                    else if bottom = selected.Left then selected.Flip().Rotate().Rotate().Rotate()
+                    else if bottom = selected.Bottom then selected.Flip().Rotate().Rotate()
+                    else if bottom = selected.Right then selected.Flip().Rotate()
+                    else failwith "Unsupported alignment"
+    (tile.Id, nextImage)
+    
+let buildImage () =
+    let connections = input() |> connections
+    let (tr, c) = connections
+                   |> List.filter (fun (_, c) -> c.Length = 2)
+                   |> List.find (fun (i, c) ->
+                       let sides = Set.union c.[0].AllSides c.[1].AllSides
+                       sides.Contains(i.Flipped.Bottom) && sides.Contains(i.Flipped.Right))
+    
+    let dataFor id =
+        connections |> List.find (fun (t, _) -> t.Id = id)
+    
+    let idGrid = Array2D.create 12 12 0
+    let grid = Array2D.create 12 12 (Image(["X"]))
+    
+    idGrid.[0,0] <- tr.Id
+    grid.[0,0] <- tr.Flipped
+    
+    for y in [0 .. 11] do
+        for x in [0 .. 11] do
+            if x = 0 && y = 0 then ()
+            else if x = 0 then
+                let (_, con) = dataFor idGrid.[x, y - 1]
+                let img = grid.[x, y - 1]
+                let (nextId, nextImg) = findBottom img con
+                idGrid.[x, y] <- nextId
+                grid.[x, y] <- nextImg
+            else
+                let (_, con) = dataFor idGrid.[x - 1, y]
+                let img = grid.[x - 1, y]
+                let (nextId, nextImg) = findRight img con
+                idGrid.[x, y] <- nextId
+                grid.[x, y] <- nextImg 
+    grid
+    
+let combineImage (grid: Image [,]) =
+    [0 .. 11]
+                |> List.map (fun y -> grid.[0..11,y] |> Array.reduce Image.MergeRight)
+                |> List.reduce Image.MergeDown
+    
+let monster() =
+    Image([
+        "                  # "
+        "#    ##    ##    ###"
+        " #  #  #  #  #  #   "
+    ])
+    
+    
+[<Fact>]
+let ``Can find all connections`` () =
+    let image = buildImage()
+    let trimmed = image |> Array2D.map (fun i -> i.Trim())
+    
+    let search = (combineImage trimmed).Flip().Rotate()
+    let searchHashes = search.Count '#'
+    
+    let monster = monster()
+    let monsterHashes = monster.Count '#'
+    
+    let monsters = search.InstancesOf monster
+    Assert.Equal(2442, searchHashes - (monsters * monsterHashes))
--- Day20/Tile.fs
+++ Day20/Tile.fs
@@ -9,13 +9,12 @@ let rev (str: string) = str.ToCharArray() |> Array.rev |> String
 
 type Image(content: string list) =
     let top = content.[0]
-    let bottom = content.[content.Length - 1]
+    let bottom = rev content.[content.Length - 1]
 
     let right =
         content
         |> List.map (fun a -> a.[a.Length - 1])
         |> List.toArray
-        |> Array.rev
         |> String
 
     let left =
@@ -25,8 +24,6 @@ type Image(content: string list) =
         |> Array.rev
         |> String
         
-    
-
     let side data = { Hash = data.GetHashCode() }
 
     member private this.Content = content
@@ -44,6 +41,7 @@ type Image(content: string list) =
     member this.Flip() =
         let flipped = content |> List.map rev
         Image(flipped)
+        
 
     member this.Rotate() =
         let sizeX = content.[0].Length
@@ -58,8 +56,31 @@ type Image(content: string list) =
            
         Image(next)
         
-    member this.Print() =
-        String.Join("\n", content)
+    member this.Trim() =
+        Image(content.[1..content.Length - 2] |> List.map (fun s -> s.[1..s.Length - 2]))
+        
+    member this.Raw = String.Join("\n", content)
+
+    member this.Count c =
+        content
+            |> List.map (fun line -> line.ToCharArray() |> Array.filter (fun a -> a = c) |> Array.length)
+            |> List.sum
+    
+    member this.SubsetMatch ((bx,by): int * int) (target: Image) =
+        let mutable m = true
+        for y in [0 ..target.Content.Length - 1] do
+            for x in [0 ..target.Content.[0].Length - 1] do
+                let self = content.[by + y].[bx + x]
+                let t = target.Content.[y].[x]
+                if t = '#' && self <> '#' then m <- false
+        m
+    
+    member this.InstancesOf (target: Image) =
+        let mutable instances = 0
+        for by in [0 .. content.Length - target.Content.Length] do
+            for bx in [0 .. content.[0].Length - target.Content.[0].Length] do
+                if this.SubsetMatch (bx, by) target then instances <- instances + 1
+        instances
         
     static member MergeRight (a: Image) (b: Image) =
         Image(a.Content |> List.mapi (fun i s -> s + b.Content.[i]))
@@ -67,8 +88,6 @@ type Image(content: string list) =
         Image(a.Content @ b.Content)
         
     
-        
-
 type Tile(id: int, image: Image) =
     member this.Id = id
     member this.Image = image
@@ -81,84 +100,6 @@ type Tile(id: int, image: Image) =
 
         intersect.Count > 0
 
-type Grid(connections: (Tile * Tile list) list) =
-
-    let findRightTile (image: Image) (possible: Tile list) =
-        let side = image.Right
-
-        let tile =
-            possible
-            |> List.find (fun i -> i.AllSides.Contains side)
-
-        let mutable next = if Set(tile.Image.Sides).Contains side then tile.Image else tile.Flipped
-        
-        while next.Left <> side do
-            next <- next.Rotate()
-
-        (tile.Id, next)
-        
-    let findBottomTile (image: Image) (possible: Tile list) =
-        let side = image.Bottom
-
-        let tile =
-            possible
-            |> List.find (fun i -> Set(i.AllSides).Contains side)
-
-        let mutable next = if Set(tile.Image.Sides).Contains side then tile.Image else tile.Flipped
-        
-        while next.Top <> side do
-            next <- next.Rotate()
-
-        (tile.Id, next)
-
-    let findConnections (id: int) =
-        let (_, c) = connections |> List.find (fun (t, _) -> t.Id = id)
-        c
-    
-    let size = int (sqrt (float connections.Length))
-
-    let grid = Array2D.create size size (Image([ " " ]))
-    let idGrid =  Array2D.create size size (0)
-
-    do
-        let (tile, cn) =
-            connections
-            |> List.filter (fun (_, c) -> c.Length = 2)
-            |> List.find (fun (t, c) ->
-                let points =
-                    c
-                    |> List.map (fun c -> c.AllSides)
-                    |> List.reduce (Set.union)
-                    |> Set
-
-                points.Contains(t.Flipped.Bottom)
-                && points.Contains(t.Flipped.Right))
-
-        let top = tile.Flipped
-        let (_, right) = findRightTile top cn
-        let (_, bottom) = findBottomTile top cn
-        
-        let image = Image.MergeDown (Image.MergeRight top right) bottom
-        let view = image.Print()
-        
-        for y in [0 .. size - 1] do
-            for x in [0 .. size - 1] do
-                if x = 0 && y = 0
-                then
-                    grid.[x,y] <- tile.Flipped
-                    idGrid.[x,y] <- tile.Id
-                else 
-                    let (id, image) =
-                        if x = 0
-                        then findBottomTile (grid.[x, y - 1]) (findConnections idGrid.[x, y - 1])
-                        else findRightTile (grid.[x - 1, y]) (findConnections idGrid.[x - 1, y])
-                    grid.[x, y] <- image
-                    idGrid.[x, y] <- id
-                
-    member this.Data = grid
-    member this.Print () =
-        Image.MergeRight grid.[0,0] grid.[1, 0]
-        
 
 let parseTile (text: string) =
     let lines =
